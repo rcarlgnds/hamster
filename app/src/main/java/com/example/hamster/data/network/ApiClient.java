3610c40f0116fa4b586c3b2e2f4c62773d430158
Rico Aurelio Gunadi S <100511874+rcarlgnds@users.noreply.github.com>
2025-07-27 14:20:35 +0700

    Fix api timeout

--- app/src/main/java/com/example/hamster/data/network/ApiClient.java
+++ app/src/main/java/com/example/hamster/data/network/ApiClient.java
@@ -1,5 +1,7 @@
 package com.example.hamster.data.network;
 
+import java.util.concurrent.TimeUnit;
+
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
@@ -12,11 +14,14 @@ public class ApiClient {
 
     public static Retrofit getClient() {
         if (retrofit == null) {
-            // Untuk logging request
             HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
             logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+
             OkHttpClient client = new OkHttpClient.Builder()
                     .addInterceptor(logging)
+                    .connectTimeout(30, TimeUnit.SECONDS)
+                    .readTimeout(30, TimeUnit.SECONDS)
+                    .writeTimeout(30, TimeUnit.SECONDS)
                     .build();
 
             retrofit = new Retrofit.Builder()
