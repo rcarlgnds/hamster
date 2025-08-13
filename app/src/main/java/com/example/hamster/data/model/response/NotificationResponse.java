b26c71ee5500b767b0a0d2f8b7d5ab66181714f0
Rico Aurelio Gunadi S <100511874+rcarlgnds@users.noreply.github.com>
2025-08-13 10:08:46 +0700

    Update NotificationResponse.java

--- app/src/main/java/com/example/hamster/data/model/response/NotificationResponse.java
+++ app/src/main/java/com/example/hamster/data/model/response/NotificationResponse.java
@@ -1,44 +1,23 @@
 package com.example.hamster.data.model.response;
 
-import android.app.Notification;
+import com.example.hamster.data.model.Notification;
+
 import java.util.List;
 
 public class NotificationResponse {
-    private int page;
-    private int limit;
-    private int total;
+    private boolean success;
+    private String message;
     private List<Notification> data;
 
-    public NotificationResponse(int page, int limit, int total, List<Notification> data) {
-        this.page = page;
-        this.limit = limit;
-        this.total = total;
-        this.data = data;
+    public boolean isSuccess() {
+        return success;
     }
 
-    public int getPage() {
-        return page;
+    public String getMessage() {
+        return message;
     }
-    public void setPage(int page) {
-        this.page = page;
-    }
-    public int getLimit() {
-        return limit;
-    }
-    public void setLimit(int limit) {
-        this.limit = limit;
-    }
-    public int getTotal() {
-        return total;
-    }
-    public void setTotal(int total) {
-        this.total = total;
-    }
+
     public List<Notification> getData() {
         return data;
     }
-    public void setData(List<Notification> data) {
-        this.data = data;
-    }
 }
-
