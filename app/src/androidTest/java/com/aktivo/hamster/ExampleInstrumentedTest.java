1056ec57887d19e88d1380ca9664bfac8998d42b
Rico Aurelio Gunadi S <100511874+rcarlgnds@users.noreply.github.com>
2025-09-15 12:16:59 +0700

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/com/aktivo/hamster/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/aktivo/hamster/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.hamster", appContext.getPackageName());
+        assertEquals("com.aktivo.hamster", appContext.getPackageName());
     }
 }
